c97fb9045265eb17261951df33f14316949e8ee9
antONget <a.l.ponomare@mail.ru>
2024-08-13 10:15:58 +0300

    перевод заказа в невостребованный

--- database/requests.py
+++ database/requests.py
@@ -1,7 +1,8 @@
 from database.models import User, Order
 from database.models import async_session
-from sqlalchemy import select, update, delete
+from sqlalchemy import select
 from dataclasses import dataclass
+from datetime import datetime
 import logging
 
 
@@ -22,7 +23,7 @@ async def add_admin(data: dict):
     """
     logging.info(f'add_admin')
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == data["tg_id"]))
+        user = await session.scalar(select(User).where(User.tg_id == int(data["tg_id"])))
         if not user:
             session.add(User(**data))
             await session.commit()
@@ -36,7 +37,7 @@ async def set_add_user(data: dict):
     """
     logging.info(f'set_add_user')
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == data["tg_id"]))
+        user = await session.scalar(select(User).where(User.tg_id == int(data["tg_id"])))
         if not user:
             token = await session.scalar(select(User).where(User.token == data["token"]))
             if token:
@@ -70,7 +71,7 @@ async def get_all_users() -> list[User]:
 
 async def get_users_role(role: str) -> list[User]:
     """
-    Получаем список всех пользователей c определенной ролью
+    Получаем список всех пользователей с определенной ролью
     :return:
     """
     logging.info(f'get_users_role')
@@ -91,9 +92,9 @@ async def get_users_role_not(role: str) -> list[User]:
     logging.info(f'get_users_role_not')
     async with async_session() as session:
         if role == UserRole.manager:
-            users = await session.scalars(select(User).where(User.is_manager != 1, User.role != UserRole.admin))
+            users = await session.scalars(select(User).where(User.is_manager != 1, User.is_admin != UserRole.admin))
         elif role == UserRole.dispatcher:
-            users = await session.scalars(select(User).where(User.is_dispatcher != 1, User.role != UserRole.admin))
+            users = await session.scalars(select(User).where(User.is_dispatcher != 1, User.is_admin != UserRole.admin))
         return users
 
 
@@ -197,7 +198,7 @@ async def get_order_id(id_order: int) -> Order:
 
 async def get_order_bitrix_id(bitrix_id: int):
     """
-    Получаем заказ по его ID  в системе bitrix
+    Получаем заказ по его ID в системе bitrix
     :param bitrix_id:
     :return:
     """
@@ -247,16 +248,19 @@ async def set_order_amount(id_order: int, amount: float):
 
 async def set_order_status(id_order: int, status: str):
     """
-    Обновляем роль пользователя
+    Обновляем статус заказа, если возвращаем его из невостребованных то обновляем еще и дату создания
     :param id_order:
     :param status:
     :return:
     """
-    logging.info(f'set_order_amount')
+    logging.info(f'set_order_status')
     async with async_session() as session:
         order: Order = await session.scalar(select(Order).where(Order.id == id_order))
-        order.status = status
-        await session.commit()
+        if order:
+            if status == OrderStatus.new:
+                order.data_create = datetime.today().strftime('%H/%M/%S/%d/%m/%Y')
+            order.status = status
+            await session.commit()
 
 
 async def set_order_reason_of_refusal(id_order: int, refusal: str):
@@ -316,186 +320,3 @@ async def set_order_work(id_order: int, tg_executor: int, data_work: str):
         order.data_work = data_work
         order.status = OrderStatus.work
         await session.commit()
-
-# async def add_referral_user(main_user_id: int, referral_user_id: int):
-#     """
-#     ```
-#     add referral_user to "referral_users":
-#         main_user_id : int   (id главного пользователя)
-#         referral_user_id: int   (id реферала)
-#     ```
-#     """
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == main_user_id))
-#         if user:
-#             if len(user.referral_users) != 0:
-#                 user.referral_users += f",{referral_user_id}"
-#                 await session.commit()
-#             else:
-#                 user.referral_users += f"{referral_user_id}"
-#                 await session.commit()
-#
-#
-# async def increase_ton_balance(tg_id: int, s: float):
-#     """
-#     ```
-#     increase ton_balance:
-#         tg_id : int
-#         s : float  (sum of toncoins)
-#     ```
-#     """
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == tg_id))
-#         if user:
-#             user.ton_balance = format(user.ton_balance + s, '.4f')
-#
-#             await session.commit()
-#
-#
-# """ ------------- GET METHODS -------------"""
-#
-#
-# async def get_balance(tg_id: int):
-#     """
-#     ```
-#     returns:
-#         ton_balance
-#     ```
-#     """
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == tg_id))
-#         if user:
-#             return user.ton_balance
-#         else:
-#             return 0
-#
-#
-# async def get_referral_link(tg_id: int):
-#     """
-#     ```
-#     returns:
-#         referral_link : str
-#     ```
-#     """
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == tg_id))
-#         if user:
-#             return user.referral_link
-#         else:
-#             return None
-#
-#
-# async def _get_username_from_id(tg_id: int):
-#     '''```code
-#     returns:
-#         username for function "get_referral_users"
-#     ```'''
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == tg_id))
-#         if user:
-#             return user.username
-#         else:
-#             return None
-#
-#
-# async def get_referral_users(tg_id: int):
-#     '''```python
-#     returns:
-#         По вашей реферальной ссылке подписались на бот {len(users)} пользователей:\n\n
-#
-#         1. @username1
-#         2. @username2
-#         3. @username3
-#     ```'''
-#     async with async_session() as session:
-#         users = (await session.scalar(select(User).where(User.id == tg_id))).referral_users
-#
-#         if users:
-#             users = users.split(",")
-#             s = f'По вашей реферальной ссылке подписались на бот {len(users)} пользователей:\n\n'
-#             c = 1
-#             for user_id in users:
-#                 s += f"{c}. @{await _get_username_from_id(int(user_id))}\n"
-#                 c += 1
-#
-#             return s
-#         else:
-#             return None
-#
-#
-# async def can_add_ref_user(tg_id) -> bool:
-#     """
-#     ```
-#     returns:
-#        True if user not in db else False
-#     ```
-#     """
-#     async with async_session() as session:
-#         users = (await session.scalars(select(User))).all()
-#         c = 0
-#         for user in users:
-#             if user:
-#                 if len(user.referral_users) != 0:
-#                     ref_users = user.referral_users.split(",")
-#                     for ref_user in ref_users:
-#                         if ref_user == str(tg_id):
-#                             c += 1
-#
-#         if c != 0:
-#             return False
-#         return True
-#
-#
-
-#
-#
-# async def get_user_from_id(user_id: int):
-#     """
-#     Получаем информацию из таблицы User по пользователю по его id телеграм
-#     """
-#     async with async_session() as session:
-#         user = await session.scalar(select(User).where(User.id == user_id))
-#         return user
-#
-#
-# async def get_user_ton_addr_by_id(user_id: int):
-#     """
-#     Получить адрес кошелька пользователя по его id телеграм
-#     param: user_id - id телеграм
-#     """
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == user_id))
-#         if user:
-#             return user.crypto_ton_addr
-#         else:
-#             return None
-#
-#
-# '''       UPDATE       '''
-#
-# @dataclass
-# class UserStatus:
-#     passed = "None"
-#     payed = "PAYED"
-#     not_payed = "NOT_PAYED"
-#     on_work = "ON_WORK"
-#
-#
-# async def update_status(user_id: int, status: UserStatus):
-#     """
-#     Обновляем статус пользователя
-#     """
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == user_id))
-#         if user:
-#             user.status = status
-#             await session.commit()
-#
-#
-# async def update_user_ton_addr(user_id: int, user_addr: str):
-#     async with async_session() as session:
-#         user: User = await session.scalar(select(User).where(User.id == user_id))
-#         if user:
-#             user.crypto_ton_addr = user_addr
-#             await session.commit()
-#
