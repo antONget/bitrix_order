b647f118c20a8412db5b02ba2aef0ba01e1d00c2
antONget <a.l.ponomare@mail.ru>
2024-08-17 12:34:47 +0300

    отчет

--- services/get_exel.py
+++ services/get_exel.py
@@ -5,7 +5,7 @@ import database.requests as rq
 
 
 async def list_users_to_exel():
-    dict_stat = {"№ п/п": [], "ID_telegram": [], "username": [], "role": []}
+    dict_stat = {"№ п/п": [], "ID_telegram": [], "username": []}
     i = 0
     list_user = await get_all_users()
     for user in list_user:
@@ -13,7 +13,6 @@ async def list_users_to_exel():
         dict_stat["№ п/п"].append(i)
         dict_stat["ID_telegram"].append(user.tg_id)
         dict_stat["username"].append(user.username)
-        dict_stat["role"].append(user.role)
     df_stat = pd.DataFrame(dict_stat)
     with pd.ExcelWriter(path='./list_user.xlsx', engine='xlsxwriter') as writer:
         df_stat.to_excel(writer, sheet_name=f'Список пользователей', index=False)
@@ -21,14 +20,22 @@ async def list_users_to_exel():
 
 async def get_report():
 
-    dict_report = {"№ п/п": [], "ID": [], "Дата создания": []}
+    dict_report = {"№ п/п": [], "ID": [], "Дата создания": [], "Дата завершения": [], "ID исполнителя": [],
+                   "username исполнителя": []}
     i = 0
-    orders = await rq.get_orders_all()
+    orders = await rq.get_orders_status(status=rq.OrderStatus.complete)
     for order in orders:
         i += 1
         dict_report["№ п/п"].append(i)
         dict_report["ID"].append(order.id)
         dict_report["Дата создания"].append(order.data_create)
+        dict_report["Дата завершения"].append(order.data_complete)
+        dict_report["ID исполнителя"].append(order.tg_executor)
+        user = await rq.get_user_tg_id(tg_id=order.tg_executor)
+        if user:
+            dict_report["username исполнителя"].append(user.username)
+        else:
+            dict_report["username исполнителя"].append('None')
     df_stat = pd.DataFrame(dict_report)
     with pd.ExcelWriter(path='./list_report.xlsx', engine='xlsxwriter') as writer:
         df_stat.to_excel(writer, sheet_name=f'Отчет', index=False)
